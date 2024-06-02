2089a2f1e9442ddb40cf298392d44ea4f2dc53e4
Thomas Bernal <thhomasgt99@gmail.com>
2024-06-01 19:26:58 -0500

    Readme

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-export function Form({ nameUser ,name, user_role, status, social_profile = [1,2], promote = true, rating = 1, last_login}) {
+export function Form({ nameUser, name, user_role = 'Aministrador', status = 'Active', social_profile = ['Facebook', 'GitHub'], promote = true, rating = 5, last_login = 'June 2024'}) {
 	const [data, setData] = useState()
 	const [formData, setFormData] = useState({
 		name: nameUser,
@@ -24,7 +24,7 @@ export function Form({ nameUser ,name, user_role, status, social_profile = [1,2]
 			body: JSON.stringify(formData)
 		})
 			.then(response => response.json())
-			.then((data) =>{
+			.then((data) => {
 				setData(data)
 				window.location.reload();
 			})
@@ -39,34 +39,32 @@ export function Form({ nameUser ,name, user_role, status, social_profile = [1,2]
 		});
 	};
 
-	
-
 	return (
 		<>
 			<form className="form formTwo" onSubmit={handleSubmit}>
 				<label htmlFor="name">Name:</label>
 				<input required type="text" name="name" value={formData.name} onChange={handleChange} />
 
-				<label htmlFor="user_role">user_role:</label>
+				<label htmlFor="user_role">User_role:</label>
 				<input required type="text" name="user_role" value={formData.user_role} onChange={handleChange} />
 
-				<label htmlFor="status">status:</label>
+				<label htmlFor="status">Escribe: Active</label>
 				<input required type="text" name="status" value={formData.status} onChange={handleChange} />
 
-				<label htmlFor="social_profile">social_profile:</label>
+				<label htmlFor="social_profile">Social_profile:</label>
 				<input required type="text" name="social_profile" value={formData.social_profile} onChange={handleChange} />
 
-				<label htmlFor="promote">promote:</label>
+				<label htmlFor="promote">Promote:</label>
 				<input required type="text" name="promote" value={formData.promote} onChange={handleChange} />
 
-				<label htmlFor="rating">rating:</label>
+				<label htmlFor="rating">Rating:</label>
 				<input required type="text" name="rating" value={formData.rating} onChange={handleChange} />
 
-				<label htmlFor="last_login">last_login:</label>
+				<label htmlFor="last_login">Last_login:</label>
 				<input required type="text" name="last_login" value={formData.last_login} onChange={handleChange} />
 
-				<label htmlFor="created_at">created_at:</label>
-				<input required type="text" name="created_at" value={formData.created_at} onChange={handleChange} />
+				{/* <label htmlFor="created_at">created_at:</label>
+				<input required type="text" name="created_at" value={formData.created_at} onChange={handleChange} /> */}
 
 				<button type="submit">Submit</button>
 			</form>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react'
 import { Form } from './form';
 import { Modal } from './modal';
 
+
 export function Main() {
 	const [data, setData] = useState([]);
 	const [text, setText] = useState(false)
+	const [date, setDate] = useState()
 
 	useEffect(() => {
 		fetch('http://localhost:3800/api/getusers')
@@ -16,6 +18,14 @@ export function Main() {
 	function toggleBtn() {
 		setText(!text)
 	}
+
+	useEffect(() => {
+		const today = new Date();
+		const formattedDate = today.toLocaleDateString('es-CO');
+		const day = formattedDate.toString();
+		setDate(day)
+	}, [])
+
 
 	return (
 		<main>
@@ -43,13 +53,73 @@ export function Main() {
 
 							<tr>
 								<td key={item._id}>{item.name}</td>
-								<td key={item._id}>{item.user_role}</td>
-								<td key={item._id}>{item.status}</td>
-								<td key={item._id}>{item.social_profile}</td>
-								<td key={item._id}>{item.promote}</td>
-								<td key={item._id}>{item.rating}</td>
-								<td key={item._id}>{item.last_login}</td>
-								<Modal nameUser={item.name} user_role={item.user_role} status={item.status} social_profile={item.social_profile} promote={item.promote} rating={item.rating} last_login={item.last_login} />
+								<td key={item._id}>
+									{
+										item.user_role == 'Aministrador' ? (
+											<div className='divUser-viewer'>
+												<i class="fa-solid fa-user"></i>
+												Aministrator
+											</div>
+										) : (
+											<div className='divUser-viewer'>
+												<i class="fa-solid fa-eye"></i>
+												Viewer
+											</div>
+										)
+									}
+								</td>
+								<td key={item._id}>
+									{
+										item.status == 'Active' ? (
+											<div className='btnStatus'>
+												<div className='green'></div>
+												<span>Active</span>
+											</div>
+										) : (
+											<div className='btnStatus'>
+												<div className='red'></div>
+												<span>Inactive</span>
+											</div>
+										)
+									}
+								</td>
+								<td key={item._id}>
+									{
+										item.social_profile ? (
+											<div className='socialImgs'>
+												<i class="fa-brands fa-facebook"></i>
+												<i class="fa-brands fa-github"></i>
+												<i class="fa-brands fa-dribbble"></i>
+												<i class="fa-brands fa-twitter"></i>
+												<i class="fa-brands fa-google"></i>
+											</div>
+										) : ''
+									}
+								</td>
+								<td key={item._id}>
+									{
+										item.promote ? (
+											<div class="toggle-component">
+												<input type="checkbox" class="toggle-button" id="element-id-name" />
+												<label for="element-id-name"></label>
+											</div>
+										) : ''
+									}
+								</td>
+								<td key={item._id}>
+									{
+										item.rating >= 4.5 ? (
+											<div>
+												<i class="fa-sharp fa-solid fa-arrow-up"></i> {item.rating}
+											</div>
+										) : (
+											<div>
+												<i class="fa-sharp fa-solid fa-arrow-down"></i> {item.rating}
+											</div>)
+									}
+								</td>
+								<td key={item._id}>{date}</td>
+								<Modal nameUser={item.name} user_role={item.user_role} status={item.status} social_profile={item.social_profile} promote={item.promote} rating={item.rating} last_login={date} />
 							</tr>
 
 						</>
@@ -57,7 +127,5 @@ export function Main() {
 				}
 			</table>
 		</main>
-
 	)
-
 }
